5fb36d48a72969199ff921273cd876b1b44ec68c
Alex da Franca <alex@farbflash.de>
2018-05-27 17:11:33 +0200

    Initial Commit

---
+++ SBEnumerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SBEnumerator
+//
+//  Created by Alex da Franca on 27.05.18.
+//  Copyright © 2018 Farbflash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
